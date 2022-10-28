5aa073c26bbd6eb2167ddc156b54443cb85216d7
tien226anh <52616756+tien226anh@users.noreply.github.com>
2022-10-28 11:30:36 +0700

    Update getImgUrls_TienAnh_bs4.py

--- getImgUrls_TienAnh_bs4.py
+++ getImgUrls_TienAnh_bs4.py
@@ -4,12 +4,11 @@ from bs4 import BeautifulSoup
 
 url = input("Nhap 1 link bai bao cu the\n")
 
-ls = []
 html  = urlopen(url)#co trong urllib.request
 bsObj = BeautifulSoup(html, "html.parser")
-imgs = bsObj.findAll("img")
+imgs = bsObj.findAll("img")#tim trong tag <img> cua html
 for img in imgs:
-    src = img['src']
+    src = img['src'] #lay attribute 'src'
     if src == "None":
       continue
     print(src)
